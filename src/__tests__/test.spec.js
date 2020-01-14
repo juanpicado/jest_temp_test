072a23ace94060ced73d7090dde62d70b9b8e21f
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2020-01-14 20:18:06 +0100

    Update test.spec.js

--- src/__tests__/test.spec.js
+++ src/__tests__/test.spec.js
@@ -1,6 +1,15 @@
+import fs from 'fs';
+import os from 'os';
+
 describe('dsadsa', () => {
 
 	test('dsadasdsadsa', () => {
+		const dd = path.join(os.tmpdir(), '/verdaccio-test');
+
+		console.log("d-->", dd);
+		const tempRoot = fs.mkdtempSync(d);
+
+
 		expect(1).toEqual(1);
 	});
 
